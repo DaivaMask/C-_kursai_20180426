43e4c1bfa4963b67a260e155bdba09f43f27331d
Your Name <you@example.com>
2018-05-04 20:20:53 +0300

    nauja tema

---
+++ 11 - 1 uzduotis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programa = new Program();
+            var vartotojo_vardas = programa.vartotojoVardas();
+            programa.pasisveikinti(vartotojo_vardas);
+
+        }
+
+        public void pasisveikinti(string vardas)
+        {
+            Console.WriteLine("o labas, " + vardas);
+        }
+
+        public string vartotojoVardas()
+        {
+            Console.WriteLine("ivesk varda: ");
+            var vardas = Console.ReadLine();
+            return vardas;
+        }
+
+    }
+}
---
+++ 11 pvz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_pvz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cia rasosi kodas
+            var programa = new Program();
+            programa.Pasisveikinti();
+            programa.Pasisveikinti();
+
+            programa.Atsisveikinti();
+
+            Pasisveikinti2();
+
+            //arba
+            Program.Pasisveikinti2(); //jeigu butu is kitos klases (virsuj class Program)
+        }
+
+        //cia rasosi metodai
+
+
+    public static void Pasisveikinti2()
+       {
+        Console.WriteLine("labas is static metodo");
+        }
+     
+
+
+
+    //metodas pasisveikinimui
+    public void Pasisveikinti()
+    {
+        Console.WriteLine("laba diena");
+    }
+
+    public void Atsisveikinti()
+    {
+        Console.WriteLine("viso gero!");
+    }
+}
+}
